6e2030663a235ff5f4d7680498467c85486a0f72
Levy Mota <levy@iMac-de-Levy.local>
2019-04-07 16:33:36 -0300

    mudanca backend

--- products/models.py
+++ products/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.name
