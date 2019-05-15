201f15ab98afeec998b82852e0beaa4341b55b67
levymoreira <levy.moreira@intmed.com.br>
2019-05-14 22:41:16 -0300

    search nos produtos

--- products/views.py
+++ products/views.py
@@ -1,28 +1,15 @@
-from django_filters import FilterSet, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.filters import SearchFilter
 from products.models import Product
 from products.serializers import ProductSerializer
-
-
-class ProductFilter(FilterSet):
-    name = filters.CharFilter('name')
-
-    class Meta:
-        model = Product
-        fields = ("__all__")
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
-    filter_backends = (DjangoFilterBackend, OrderingFilter, SearchFilter)
-    filter_class = ProductFilter
-    filter_fields = ('name',)
-    ordering_fields = ('name',)
-    ordering = ('name')
-    search_fields = ('name','name')
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    search_fields = ('^name','^name')
 
 
 
--- sells/views.py
+++ sells/views.py
@@ -1,13 +1,19 @@
+from django_filters.rest_framework import DjangoFilterBackend, OrderingFilter
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
+
 from sells.models import Sell
 from sells.serializers import SellSerializer, SellListSerializer
 from rest_framework.response import Response
 from rest_framework import mixins
 
 
+
 class SellViewSet(viewsets.ModelViewSet, mixins.CreateModelMixin):
     queryset = Sell.objects.all()
     serializer_class = SellSerializer
+    filter_backends = (DjangoFilterBackend, OrderingFilter, SearchFilter)
 
     # def create(self, request, *args, **kwargs):
     #     serializer = SellSerializer(request.data)
@@ -16,6 +22,10 @@ class SellViewSet(viewsets.ModelViewSet, mixins.CreateModelMixin):
     def list(self, request):
         serializer = SellListSerializer(Sell.objects.all().order_by("date").reverse(), many=True)
         return Response(serializer.data)
+
+    @action(methods=['get'], detail=True, url_path="buscar", url_name="buscar")
+    def buscar(self, request):
+        pass
 
 
 
