66da60dabb61ccd1b5c7ebcd945446b9773a3c83
Levy Mota <levy@iMac-de-Levy.local>
2019-09-21 00:25:09 -0300

    adicionado o campo de unidade no produto

--- products/models.py
+++ products/models.py
@@ -3,13 +3,21 @@ from category.models import Category
 
 
 class Product(models.Model):
+
+    UNITY_CHOICES = (
+	("KG","Kg"),
+        ("SC","SC"),
+	("UND","UND")
+	)    
+
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=7, decimal_places=3)
     date = models.DateField()
     description = models.CharField(max_length=250)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     quantity = models.DecimalField(max_digits=6, decimal_places=2)
-    
+    unity = models.CharField(max_length=5,choices=UNITY_CHOICES,default="KG")
+ 
     def __str__(self):
         return self.name
 
